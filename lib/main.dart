36cacb87d2ba7a841f77ab43e6764c6e82bed735
JUANIMAN <juanimandarin9@gmail.com>
2024-09-14 15:13:49 -0600

    migrated to multiple providers

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:manager/app.dart';
 import 'package:manager/config/theme_provider.dart';
+import 'package:manager/services/system_service.dart';
 import 'package:provider/provider.dart';
 
 void main() {
   runApp(
-    ChangeNotifierProvider(
-      create: (_) => ThemeProvider(),
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => ThemeProvider()),
+        ChangeNotifierProvider(create: (_) => SystemService()),
+      ],
       child: const MyApp(),
     ),
   );
 }
-

3aa8c95accb1941e1ef9f7fec8197aef26e4c4a3
JUANIMAN <juanimandarin9@gmail.com>
2024-12-25 14:35:57 -0600

    remove unnecessary application first launch logic

--- lib/config/theme_provider.dart
+++ lib/config/theme_provider.dart
@@ -6,7 +6,6 @@ import 'package:shared_preferences/shared_preferences.dart';
 class ThemeProvider with ChangeNotifier {
   ThemeMode _themeMode = ThemeMode.system;
   static const String _themeModeKey = 'themeMode';
-  static const String _firstLaunchKey = 'isFirstLaunch';
 
   ThemeMode get themeMode => _themeMode;
 
@@ -16,20 +15,16 @@ class ThemeProvider with ChangeNotifier {
 
   Future<void> _loadThemePreference() async {
     final prefs = await SharedPreferences.getInstance();
-    bool isFirstLaunch = prefs.getBool(_firstLaunchKey) ?? true;
+    final savedThemeMode = prefs.getString(_themeModeKey);
 
-    if (isFirstLaunch) {
-      _themeMode = ThemeMode.system;
-      await prefs.setBool(_firstLaunchKey, false);
-    } else {
-      final savedThemeMode = prefs.getString(_themeModeKey);
-      _themeMode = savedThemeMode != null
-          ? ThemeMode.values.firstWhere(
+    _themeMode = savedThemeMode != null
+        ? ThemeMode.values.firstWhere(
             (e) => e.toString() == savedThemeMode,
-        orElse: () => ThemeMode.light,
-      )
-          : ThemeMode.light;
-    }
+            orElse: () => ThemeMode.light,
+          )
+        : ThemeMode.system;
+
+    _updateSystemUIOverlayStyle();
     notifyListeners();
   }
 
@@ -41,22 +36,23 @@ class ThemeProvider with ChangeNotifier {
       final prefs = await SharedPreferences.getInstance();
       await prefs.setString(_themeModeKey, _themeMode.toString());
     }
-    _updateSystemUIOverlayStyle();
   }
 
   void toggleTheme() {
-    setThemeMode(_themeMode == ThemeMode.light ? ThemeMode.dark : ThemeMode.light);
+    setThemeMode(
+        _themeMode == ThemeMode.light ? ThemeMode.dark : ThemeMode.light);
   }
 
   void _updateSystemUIOverlayStyle() {
     bool isDark = _themeMode == ThemeMode.dark ||
-        (_themeMode == ThemeMode.system && WidgetsBinding.instance.platformDispatcher.platformBrightness == Brightness.dark);
+        (_themeMode == ThemeMode.system &&
+            WidgetsBinding.instance.platformDispatcher.platformBrightness ==
+                Brightness.dark);
 
     SystemChrome.setSystemUIOverlayStyle(
       isDark ? SystemUiOverlayStyle.light : SystemUiOverlayStyle.dark,
     );
   }
-
 }
 
 class ThemeSwitcher extends StatelessWidget {

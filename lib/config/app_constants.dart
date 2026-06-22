1c56ac6c15b608af5e134ee600f88cf1657ead75
JUANIMAN <juanimandarin9@gmail.com>
2026-06-22 17:29:44 -0600

    chore: bump version to 6.0.0+1 and add google_fonts dependency

--- lib/config/app_constants.dart
+++ lib/config/app_constants.dart
@@ -2,11 +2,13 @@ import 'package:flutter/material.dart';
 
 /// Constantes
 class AppConstants {
-  // Espaciado
+  // Spacing
   static const double spacing4 = 4.0;
   static const double spacing6 = 6.0;
   static const double spacing8 = 8.0;
+  static const double spacing10 = 10.0;
   static const double spacing12 = 12.0;
+  static const double spacing14 = 14.0;
   static const double spacing16 = 16.0;
   static const double spacing20 = 20.0;
   static const double spacing24 = 24.0;
